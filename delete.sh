e16ceb6450967e895f030fb719d963fa7c8fec16
Justin Davis <justin.davis@ba.com>
2016-09-21 21:49:28 +0200

    keycloak s2i stuff

---
+++ delete.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+oc delete pod keycloak-1-build
+oc delete build keycloak-1-build
+oc delete buildconfig keycloak
+oc delete deploymentconfig keycloak
+oc delete service keycloak
+oc delete imagestream keycloak
+oc delete route keycloak
